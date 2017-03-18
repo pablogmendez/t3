31cf172197c419a0c895bf297b488cf9c798ae1a
pablo <pablo.23.32.23@gmail.com>
2017-03-18 15:54:56 -0300

    add proyect maven structure

---
+++ loadTestConsole/src/main/java/ar/fiuba/taller/loadTestConsole/App.java
@@ -0,0 +1,43 @@
+package ar.fiuba.taller.loadTestConsole;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 Properties propiedades = new Properties();
+    	    InputStream entrada = null;
+
+    	    try {
+
+    	        entrada = new FileInputStream("src/main/resources/configuracion.properties");
+
+    	        // cargamos el archivo de propiedades
+    	        propiedades.load(entrada);
+
+    	        // obtenemos las propiedades y las imprimimos
+    	        System.out.println(propiedades.getProperty("basedatos"));
+    	        System.out.println(propiedades.getProperty("usuario"));
+    	        System.out.println(propiedades.getProperty("clave"));
+
+    	    } catch (IOException ex) {
+    	        ex.printStackTrace();
+    	    } finally {
+    	        if (entrada != null) {
+    	            try {
+    	                entrada.close();
+    	            } catch (IOException e) {
+    	                e.printStackTrace();
+    	            }
+    	        }
+    	    }
+    }
+}
